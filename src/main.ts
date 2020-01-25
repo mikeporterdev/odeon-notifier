7d5e1236a164b97239cc7dac17c1d46b2b02c033
Michael Porter <m_porter@live.co.uk>
2020-01-25 15:38:19 +0000

    Basic scraping done

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,129 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import * as puppeteer from 'puppeteer';
+import { Browser, Page } from 'puppeteer';
+import { addHours, parse } from 'date-fns';
+import { install } from 'source-map-support';
+
+install();
+
+interface Movie {
+  title: string;
+  dates: Date[];
 }
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+interface PuppeteerMovie {
+  title: string;
+  dates: string[];
 }
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing missing return type definitions for greeter function
+export class OdeonScraper {
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
+  private browser: Browser;
+
+  public async scrapeOdeon(): Promise<void> {
+
+    const page = await this.checkBrowserRunning();
+    await page.goto('https://www.odeon.co.uk/cinemas/glasgow_quay/120/');
+    await page.click('[href=\\#ind-week]');
+    const byWeek = await this.parseFilms(page, 'WEEK');
+    await page.click('[href=\\#ind-future]');
+    await page
+      .waitForSelector('.futureview > .tab-content > .tab-pane > div');
+    const advanceDates = await this.parseFilms(page, 'FUTURE');
+
+    const fullList =
+      [
+        ...byWeek,
+        ...advanceDates,
+      ];
+
+    const allFilms = fullList.reduce((acc, film) => {
+      const existingFilm = acc.find(i => i.title === film.title);
+
+      if (existingFilm) {
+        existingFilm.dates = [...existingFilm.dates, ...film.dates];
+      } else {
+        acc.push(film);
+      }
+      return acc;
+
+    }, [] as PuppeteerMovie[]);
+
+    const parseDatesMovies: Movie[] = allFilms.map(i => {
+      return {
+        title: i.title,
+        dates: i.dates.map(date => {
+          // I don't actually care about the times, so to make sure the dates are correct, just add one hour in case of DST
+          return addHours(parse(date.slice(-6), 'dd MMM', new Date()), 1);
+        }),
+      };
+    });
+
+    const filteredCategories = parseDatesMovies
+      .filter(film => !film.title.includes('Autism Friendly'))
+      .filter(film => !film.title.includes('Dubbed'));
+
+    console.log(filteredCategories);
+  }
+
+
+  private async checkBrowserRunning(): Promise<Page> {
+    if (!this.browser?.isConnected()) {
+      await this.browser?.close();
+      this.browser = await puppeteer.launch({
+        // headless: false,
+        // defaultViewport: {
+        //   width: 1920,
+        //   height: 1080,
+        // },
+      });
+    }
+
+    return await this.browser.newPage();
+  }
+
+  private async parseFilms(page: Page, type: string): Promise<PuppeteerMovie[]> {
+    let className = type.toLowerCase();
+    className = (className.includes('day')) ? 'day' : className;
+
+    await page
+      .waitForSelector(`.${className}view > .tab-content > .tab-pane > div > #ind-film-list-${type}`);
+    return await page.evaluate((type) => {
+      const filmList: PuppeteerMovie[] = [];
+      const allFilmDetails = document.querySelectorAll(`.film-detail.${type}`);
+
+      allFilmDetails.forEach(filmDetailDiv => {
+        const title = filmDetailDiv.querySelector('.presentation-info > h4 > a').textContent
+
+        const dateDivs = filmDetailDiv.querySelectorAll('.times > .presentation-info');
+
+        const dates: string[] = [];
+        dateDivs.forEach(dateDiv => {
+          // dates.push(dateDiv.textContent);
+          dates.push(dateDiv.textContent);
+        });
+
+        const film = {
+          title,
+          dates,
+        };
+
+        filmList.push(film);
+      });
+      return Promise.resolve(filmList);
+    }, type);
+  }
 }
+
+const odeonScraper = new OdeonScraper();
+
+(async () => {
+  await odeonScraper.scrapeOdeon().catch(e => console.error(e));
+})();
+
+// new CronJob('* * * * * *', async () => {
+//   await odeonScraper.scrapeOdeon().catch(e => console.error(e));
+// }, null, true, 'Europe/London')
+
+
+// Keep process alive
+new Promise(() => null);
